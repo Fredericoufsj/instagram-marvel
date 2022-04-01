8ee2184c72122b42787be408604fdb969de79cb9
Frederico Thomaz <fredericoufsj@yahoo.com.br>
2022-04-01 14:02:27 -0300

    Ligação com bancos H2 e Postgres

--- backend/src/main/java/com/supersim/apisimples/controllers/UserController.java
+++ backend/src/main/java/com/supersim/apisimples/controllers/UserController.java
@@ -3,7 +3,10 @@ package com.supersim.apisimples.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +14,8 @@ import com.supersim.apisimples.entities.Users;
 import com.supersim.apisimples.repositories.UserRepository;
 
 @RestController
-@RequestMapping(value = "/")
+@CrossOrigin(origins = "*")
+@RequestMapping(value = "/users")
 public class UserController {
 
 	@Autowired
@@ -21,4 +25,9 @@ public class UserController {
 	public List<Users> getAll(){
 		return repository.findAll();
 	}
+	
+	@PostMapping(value = "/salvar")
+    public Users create(@RequestBody Users user) {
+        return repository.save(user);
+    }
 }
